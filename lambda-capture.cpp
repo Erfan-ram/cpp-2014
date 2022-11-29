2fa89ff61ccfac0b728d404889510a3185571b58
Erfan-ram <Erfanramezany245@gmail.com>
2022-11-29 19:44:34 +0330

    capture pass by Refrence added

--- lambda-capture.cpp
+++ lambda-capture.cpp
@@ -1,18 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
-  int initial_sum = 100;
+    int initial_sum = 100;
 
-  // capture initial_sum by value
-  auto add_to_sum = [initial_sum] (int num) {
-    // here inital_sum = 100 from local scope
-    return initial_sum + num;
-  };
+    // capture initial_sum by value
+    auto add_to_sum = [initial_sum](int num)
+    {
+        // here inital_sum = 100 from local scope
+        return initial_sum + num;
+    };
 
-  int final_sum = add_to_sum(78);
-  cout << "100 + 78 = " << final_sum;
+    int final_sum = add_to_sum(78);
+    cout << "100 + 78 = " << final_sum;
 
-  return 0;
+    // access the address of num_main variable
+    auto changeValue = [&initial_sum]()
+    {
+        initial_sum = 900;
+    };
+
+    changeValue();
+
+    cout
+        << endl
+        << initial_sum;
+
+    return 0;
 }
