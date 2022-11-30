2517874606d416572313045d883ae51941484bae
Erfan-ram <Erfanramezany245@gmail.com>
2022-11-30 15:54:47 +0330

    develop declare type

--- declr-type.cpp
+++ declr-type.cpp
@@ -3,8 +3,18 @@ using namespace std;
 
 int main()
 {
+    // c++ 11 feature
+    int a = 3, b = 5;
+    float flt = 2.5;
+    string str = "text";
 
-    int k = 5;
+    cout << typeid(a).name() << "   "
+         << "int";
 
+    cout << typeid(str).name() << "   "
+         << "int";
+
+    cout << typeid(flt).name() << "   "
+         << "int";
     return 0;
 }
