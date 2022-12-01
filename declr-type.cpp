292746f510d140c292fbbf8921d02c9ead3ee53f
Erfan-ram <Erfanramezany245@gmail.com>
2022-12-01 19:37:28 +0330

    major update

--- declr-type.cpp
+++ declr-type.cpp
@@ -4,33 +4,39 @@ using namespace std;
 
 int main()
 {
-    // c++ 11 feature
-    int a = 3, b = 5;
-    float flt = 2.5;
-    string str = "text";
-    char chr = 'c';
-    char ch[100] = "abcd";
+     // c++ 11 feature
+     const char test = 'i';
+     int a = 3, b = 5;
+     float flt = 2.5;
+     string str = "text";
+     char chr = 'c';
+     char ch[100] = "abcd";
 
-    cout << typeid(a).name() << "   "
-         << "int" << endl;
+     cout << (*typeid(a).name() == 'i') << "   "
+          << "int" << endl;
 
-    cout << typeid(str).name() << "   "
-         << "string" << endl;
+     cout << *typeid(str).name() << "   "
+          << "string" << endl;
 
-    cout << typeid(flt).name() << "   "
-         << "float" << endl;
+     cout << *typeid(flt).name() << "   "
+          << "float" << endl;
 
-    cout << typeid(chr).name() << "   "
-         << "char" << endl;
+     cout << *typeid(chr).name() << "   "
+          << "char" << endl;
 
-    cout << typeid(ch).name() << "   "
-         << "char list" << endl;
+     cout << typeid(ch).name() << "   "
+          << "char list" << endl;
+     cout << *typeid(ch).name() << "   "
+          << "char list" << endl;
 
-    // ---------- //
-    float ft = 4.8;
+     // ---------- //
+     float ft = 4.8;
 
-    decltype(ft) expl = ft + 9.8;
-    cout << typeid(expl).name();
+     decltype(ft) expl = ft + 9.8;
+     cout << typeid(expl).name();
 
-    return 0;
+     // ---------- //
+     cout << typeid(typeid(expl).name()).name();
+
+     return 0;
 }
