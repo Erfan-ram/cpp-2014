3dd1c59fd79018f88b6c6fbab66025b3e0184d59
Erfan-ram <Erfanramezany245@gmail.com>
2022-12-06 19:02:10 +0330

    added aggregate

---
+++ files/aggregate.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+struct Employee
+{
+    int id;
+    int age;
+    double wage;
+};
+
+int main()
+{
+    Employee frank = {1, 32, 60000.0}; // copy-list initialization using braced list
+    Employee joe{2, 28, 45000.0};      // list initialization using braced list (preferred)
+
+    cout << (frank.age > joe.age);
+    return 0;
+}
