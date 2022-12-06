1a0079ad4078eb9940fb08a44b362ec8e1546a2a
Erfan-ram <Erfanramezany245@gmail.com>
2022-12-06 15:54:48 +0330

    added vector push

--- vector.cpp
+++ vector.cpp
@@ -52,5 +52,28 @@ int main()
         cout << vector3[i] << "  ";
     }
 
+    // -------------------------------
+    // ------ push to a vectore ---------------------------
+    // -------------------------------
+    vector<int> num{1, 2, 3, 4, 5};
+
+    cout << "\n\npush to a vectore: ";
+
+    for (const int &i : num)
+    {
+        cout << i << "  ";
+    }
+
+    // add the integers 6 and 7 to the vector
+    num.push_back(6);
+    num.push_back(7);
+
+    cout << "\nUpdated Vector: ";
+
+    for (const int &i : num)
+    {
+        cout << i << "  ";
+    }
+
     return 0;
 }
